ba5ce8277ecb5debda37239c8730dcb5478c8279
Abhishek Das <abhishek2022.work@gmail.com>
2023-07-24 18:33:55 +0530

    done

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL : 'http://localhost:5000'})
+const API = axios.create({baseURL : 'https://mern-app-j3o1.onrender.com'})
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
